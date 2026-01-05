c190b7d35f10354860d600aa0cb92748479a4ad6
Ayrton <ayrton.silva@mps.com.br>
2026-01-05 15:44:15 -0300

    Tychus lua update

--- Lua/Heroes/Tychus/Arsenal Upgrade/ArsenalUpgrade.lua
+++ Lua/Heroes/Tychus/Arsenal Upgrade/ArsenalUpgrade.lua
@@ -1,38 +1,46 @@
---[[ requires RegisterPlayerUnitEvent
-    /* -------------------- Arsenal Upgrade v1.2 by Chopinski ------------------- */
-    // Credits:
-    //     Magtheridon96  - RegisterPlayerUnitEvent
-    /* ----------------------------------- END ---------------------------------- */
-]]--
+OnInit("ArsenalUpgrade", function (requires)
+    requires "Class"
+    requires "Spell"
+    requires "Utilities"
+    requires "RegisterPlayerUnitEvent"
 
-do
-    -- -------------------------------------------------------------------------- --
-    --                                Configuration                               --
-    -- -------------------------------------------------------------------------- --
+    -- --------------------------- Arsenal Upgrade v1.3 by Chopinski --------------------------- --
+
+    -- ----------------------------------------------------------------------------------------- --
+    --                                       Configuration                                       --
+    -- ----------------------------------------------------------------------------------------- --
     -- The raw code of the Arsenal Upgrade Ability
-    ArsenalUpgrade_ABILITY  = FourCC('A00B')
+    ArsenalUpgrade_ABILITY  = S2A('TycB')
     -- The raw code of the Tychus unit in the editor
-    local TYCHUS_ID         = FourCC('E000')
+    local TYCHUS_ID         = S2A('Tycs')
     -- The GAIN_AT_LEVEL is greater than 0
     -- Tychus will gain Arsenal Upgrade at this level 
     local GAIN_AT_LEVEL     = 20
 
-    -- -------------------------------------------------------------------------- --
-    --                                   System                                   --
-    -- -------------------------------------------------------------------------- --
-    ArsenalUpgrade = setmetatable({}, {})
-    local mt = getmetatable(ArsenalUpgrade)
-    mt.__index = mt
-    
-    onInit(function()
-        RegisterPlayerUnitEvent(EVENT_PLAYER_HERO_LEVEL, function()
+    -- ----------------------------------------------------------------------------------------- --
+    --                                           System                                          --
+    -- ----------------------------------------------------------------------------------------- --
+    do
+        ArsenalUpgrade = Class(Spell)
+
+        function ArsenalUpgrade:onTooltip(source, level, ability)
+            return "|cffffcc00Tychus|r non ultimate abilities are upgraded.\n\n|cffffcc00Frag Granade|r - Enemy units cought in the explosion radius are stunned for |cffffcc001.5|r seconds.\n\n|cffffcc00Automated Turrent|r - Reduces the number of attacks necessary to the turrents to release a missile by |cffffcc004|r.\n\n|cffffcc00Overkill|r - The mana cost per bullet is halved.\n\n|cffffcc00Run and Gun|r - Doubles the duration of the movement speed bonus."
+        end
+
+        function ArsenalUpgrade.onLevelUp()
+            local unit = GetTriggerUnit()
+        
             if GAIN_AT_LEVEL > 0 then
-                local unit = GetTriggerUnit()
                 if GetUnitTypeId(unit) == TYCHUS_ID and GetHeroLevel(unit) == GAIN_AT_LEVEL then
                     UnitAddAbility(unit, ArsenalUpgrade_ABILITY)
                     UnitMakeAbilityPermanent(unit, true, ArsenalUpgrade_ABILITY)
                 end
             end
-        end)
-    end)
-end
+        end
+
+        function ArsenalUpgrade.onInit()
+            RegisterSpell(ArsenalUpgrade.allocate(), ArsenalUpgrade_ABILITY)
+            RegisterPlayerUnitEvent(EVENT_PLAYER_HERO_LEVEL, ArsenalUpgrade.onLevelUp)
+        end
+    end
+end)
